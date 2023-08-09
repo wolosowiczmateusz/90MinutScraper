fe1c10e648d9197c41892105cfc291f167c61c07
Mateusz Wołosowicz <80713@student.pb.edu.pl>
2023-08-09 15:14:55 +0200

    added cors

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const request = require('request');
 const cheerio = require('cheerio');
 const app = express();
-const puppeteer = require("puppeteer");
 const axios = require("axios");
 const iconv = require('iconv-lite');
 
@@ -17,59 +16,11 @@ app.get('/', async(req, res)=>{
 app.listen('8080');
 console.log('API is running on http://localhost:8080');
 
-const scrapeTeams = async(res) =>{
-    const browser = await puppeteer.launch({headless: "new"});
-    const page = await browser.newPage();
-    await page.goto("http://www.90minut.pl/liga/1/liga12974.html");
-    const grabTable = await page.evaluate(() =>{
-        const teamsTable = document.querySelectorAll(".main2 tbody tr td")
-        let teamsString = [];
-        let teams = [];
-        teamsTable.forEach((team)=>{
-            teamsString.push(team.innerText)
-            console.log(team.innerText);
-        })
-
-        teamsString = teamsString.slice(29,425)
-        for(let i = 0; i<18;i++){
-            let teamObject = {
-                position: teamsString[0],
-                name: teamsString[1],
-                matchesPlayed: teamsString[2],
-                points: teamsString[3],
-                wins: teamsString[4],
-                draws: teamsString[5],
-                loses: teamsString[6],
-                goals: teamsString[7],
-                statsHome: {
-                    wins: teamsString[8],
-                    draws: teamsString[9],
-                    loses: teamsString[10],
-                    goals: teamsString[11]
-                },
-                statsAway: {
-                    wins: teamsString[12],
-                    draws: teamsString[13],
-                    loses: teamsString[14],
-                    goals: teamsString[15]
-                }
-            };
-            teamsString = teamsString.slice(22)
-            teams.push(teamObject)
-        }
-        return teams;
-    });
-    await browser.close();
-    return grabTable;
-
-}
-
 async function scrapeData(numOfTeams) {
-      // Fetch HTML of the page we want to scrape
       const response = await axios({
         method: 'get',
         url: 'http://www.90minut.pl/liga/1/liga12496.html',
-        responseType: 'arraybuffer'  // Ustawienie typu odpowiedzi na arraybuffer
+        responseType: 'arraybuffer'
       });
       const data = iconv.decode(response.data, 'iso-8859-2');
       const $ = cheerio.load(data);
@@ -82,7 +33,6 @@ async function scrapeData(numOfTeams) {
       let teams = [];
       teamsTable.each((idx, el)=>{
         teamsString.push($(el).text())
-        console.log($(el).text())
       })
       
       for(let i = 0; i<18;i++){
